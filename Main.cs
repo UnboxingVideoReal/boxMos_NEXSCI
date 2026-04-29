8dc6c54fef86b4cea62046c98add4bc672350e6c
282355 <282355@ST-34948>
2026-04-29 13:02:37 -0500

    yresa dat aint working

--- Main.cs
+++ Main.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Diagnostics;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace boxMos_NEXSCI
 {
@@ -13,14 +14,17 @@ namespace boxMos_NEXSCI
     {
         public static string query = "select pl_name,avg(ra),avg(dec) from PS group by pl_name";
         public static string url = $"https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query={query}";
+
+        public static List<string> ARRAYOFPLANETS = new List<string>();
         public static async void Setup()
         {
             using (HttpClient client = new HttpClient())
             {
                 string result = await client.GetStringAsync(url);
-                XmlDocument xml = new XmlDocument();
-                xml.LoadXml(result);
+                XDocument xml = XDocument.Load(result);
                 Debug.WriteLine(result);
+
+                var elements = xml.Descendants("VOTABLE").Elements("RESOURCE").Elements("TABLE").Elements("DATA").Elements("TABLEDATA").Elements("TR").Elements("TD");
             }
         }
     }
