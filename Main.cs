8875da14160da22f990999d9ea270828de942c9a
ivy <88110899+UnboxingVideoReal@users.noreply.github.com>
2026-04-29 20:17:48 -0500

    wawawa

--- Main.cs
+++ Main.cs
@@ -21,10 +21,16 @@ namespace boxMos_NEXSCI
             using (HttpClient client = new HttpClient())
             {
                 string result = await client.GetStringAsync(url);
-                XDocument xml = XDocument.Load(result);
+                XDocument xml = XDocument.Parse(result);
                 Debug.WriteLine(result);
 
-                var elements = xml.Descendants("VOTABLE").Elements("RESOURCE").Elements("TABLE").Elements("DATA").Elements("TABLEDATA").Elements("TR").Elements("TD");
+                // yea no idk what im doing i saw this on a stackoverflow thing and i tried to recreate it 
+                /* what i want is to get each entry in the table (see "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select pl_name,avg(ra),avg(dec) from PS group by pl_name" for the xml fiole)
+                   and put it in the ARRAYOFPLANETS list but idk the dimensions of the table idk what the table looks like at all idk how to interpret this xml
+                */
+                var elements = xml.Descendants("VOTABLE").Elements("RESOURCE").Elements("TABLE").Elements("DATA").Elements("TABLEDATA").Elements("TR").Elements("TD").ToString();
+
+                Debug.WriteLine(elements);
             }
         }
     }
